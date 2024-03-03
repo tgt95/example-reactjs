5797a61c54f10c46bb408666dc42f4812db8cb09
Tuan <giatuantrinh@gmail.com>
2024-03-03 09:31:28 +0700

    Update section: Profile, Work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/layouts/header';
 import Profile from './views/home/profile.js';
 import Banner from './views/home/banner.js';
+import Work from './views/home/work.js';
 
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			<Header />
 			<Banner />
 			<Profile />
+			<Work />
 		</div>
 	);
 }
--- src/views/home/profile.js
+++ src/views/home/profile.js
@@ -1,84 +1,103 @@
+import React, { Component } from "react"
 import { ReactComponent as IllusProfile } from '../../assets/images/profile-illustration.svg';
 
-const ExternalUI = (props) => {
-	return (
-		<div className="align-items-center d-inline-flex">
-			<a
-				className="me-1"
-				href={`mailto:${props.value}`}
-				style={{ textDecoration: 'underline' }}
-			>
-				{props.value}
-			</a>
-			<i className="ri-mail-open-line" style={{ fontSize: '20px', color: 'var(--neutral-700)' }}></i>
-		</div>
-	);
-}
+// Main components
+class Profile extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: null,
+			loading: true,
+			error: null
+		};
+		this.renderView = this.renderView.bind(this);
+	}
 
-const HomeProfile = () => {
-
-	const data_profile = [
-		{
-			label: 'Full name',
-			value: 'Trình Gia Tuấn',
-			external: false
-		},
-		{
-			label: 'Date of Birth',
-			value: '11/10/1995',
-			external: false
-		},
-		{
-			label: 'Email',
-			value: 'giatuantrinh@gmail.com',
-			external: true
-		},
-		{
-			label: 'Address',
-			value: 'District 11, Ho Chi Minh City',
-			external: false
-		},
-	]
-
-	return (
-		<section className="section section-profile">
-			<div className="container">
-				<div className="row align-items-center">
-					<div className="col-md-7 col-lg-6 content-bg">
-						<IllusProfile />
+	renderView = (type, value) => {
+		switch (type) {
+			case 'email':
+				return (
+					<div className="align-items-center d-inline-flex">
+						<a
+							className="me-1"
+							href={`mailto:${value}`}
+							style={{ textDecoration: 'underline' }}
+						>
+							{value}
+						</a>
+						<i className="ri-mail-open-line" style={{ fontSize: '20px', color: 'var(--neutral-700)' }}></i>
 					</div>
-					<div className="col-md-5 col-lg-6 content-text">
-						<div className="section-heading">
-							<div className="section-title">Profile</div>
+				);
+			case 'dob':
+				return (
+					<span>{`${value} (age ${(new Date().getFullYear()) - value.substr(value.length - 4)})`}</span>
+				);
+			default:
+				return <span>{value}</span>;
+		}
+	}
+
+	componentDidMount() {
+		this.fetchData('https://api.npoint.io/005fe15af1ec9c16814c');
+	}
+
+	async fetchData(URL) {
+		try {
+			const response = await fetch(URL);
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			const jsonData = await response.json();
+			this.setState({ data: jsonData, loading: false });
+		} catch (error) {
+			this.setState({ error: error.message, loading: false });
+		}
+	}
+
+	render() {
+		const { data, loading, error } = this.state;
+		if (loading) return <div>Loading...</div>;
+		if (error) return <div>Error: {error}</div>;
+		return (
+			<section className="section section-profile">
+				<div className="container">
+					<div className="row align-items-center">
+						<div className="col-md-7 col-lg-6 content-bg">
+							<IllusProfile />
 						</div>
-						<div className="profile-content">
-							<ul>
-								{data_profile.map((item, index) => (
-									<li key={index}>
-										<div className="title">{item.label}</div>
-										{!item.external ? <span>{item.value}</span> : <ExternalUI value={item.value} />}
+						<div className="col-md-5 col-lg-6 content-text">
+							<div className="section-heading">
+								<div className="section-title">Profile</div>
+							</div>
+							<div className="profile-content">
+								<ul>
+									{data && Object.keys(data).map((item, index) => (
+										<li key={index}>
+											<div className="title">{data[item].label}</div>
+											{this.renderView(data[item].type, data[item].value)}
+										</li>
+									))}
+									<li className="align-items-center">
+										<div className="personality-progress">
+											<div className="label">
+												<span>
+													<b>Introvert </b>(INFP)
+												</span>
+												<span>Extrovert</span>
+											</div>
+											<div className="holder">
+												<div className="tracker"></div>
+												<span className="emoji">👀</span>
+											</div>
+										</div>
 									</li>
-								))}
-								<li class="align-items-center">
-									<div class="personality-progress">
-										<div class="label">
-											<span>
-												<b>Introvert </b>(INFP)
-											</span>
-											<span>Extrovert</span>
-										</div>
-										<div class="holder">
-											<div class="tracker"></div>
-											<span class="emoji">👀</span>
-										</div>
-									</div>
-								</li>
-							</ul>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-	);
+			</section>
+		);
+	}
 }
-export default HomeProfile;
+export default Profile;
